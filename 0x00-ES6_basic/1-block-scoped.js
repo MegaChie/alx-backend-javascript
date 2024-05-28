e3338e4982ab5541bbae4daf78867c47ead73ceb
MegaChie <workspace.mega@gmail.com>
2024-05-28 00:20:06 +0000

    Fix linter error

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -5,6 +5,10 @@ export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
+    const trig = 1;
+    if (trig === 0) {
+      console.log(task, task2);
+    }
   }
 
   return [task, task2];
