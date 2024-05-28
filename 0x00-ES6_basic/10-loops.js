2b4fe0f88288a7a4784110f76c4fe9bac8bbc39a
MegaChie <workspace.mega@gmail.com>
2024-05-28 00:46:14 +0000

    Fix ESlint error

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const newArr = [];
   for (const value of array) {
     const idx = array.indexOf(value);
-    array[idx] = appendString + value;
+    newArr[idx] = appendString + value;
   }
 
-  return array;
+  return newArr;
 }
