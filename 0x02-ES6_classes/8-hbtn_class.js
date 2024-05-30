297576330ee46a6b4af45936038923808f18a585
MegaChie <workspace.mega@gmail.com>
2024-05-30 15:47:41 +0000

    Class cast

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,32 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(siz) {
+    this._size = siz;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(loc) {
+    this._location = loc;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return null;
+  }
+}
