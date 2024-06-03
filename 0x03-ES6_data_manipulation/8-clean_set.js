30c985647fb24e7af53283fdf5174e320cb75ece
MegaChie <workspace.mega@gmail.com>
2024-06-03 23:28:36 +0000

    Returns set elements without passed string if it starts with it

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+  let res = '';
+  if (!startString || !startString.length) return res;
+  for (const elem of set) {
+    if (elem && elem.startsWith(startString)) {
+      res += `${elem.slice(startString.length)}-`;
+    }
+  }
+  return res.slice(0, res.length - 1);
+}
