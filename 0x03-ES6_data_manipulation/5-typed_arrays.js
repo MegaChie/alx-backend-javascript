b97fdd908db0465cde63dab14f605c999cab423b
MegaChie <workspace.mega@gmail.com>
2024-06-03 22:14:32 +0000

    Returns a new ArrayBuffer with an Int8 value at specific position

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,8 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) throw Error('Position outside range');
+  const buff = new ArrayBuffer(length);
+  const view = new DataView(buff, 0, length);
+  const arr = new Int8Array(buff);
+  arr[position] = value;
+  return view;
+}
