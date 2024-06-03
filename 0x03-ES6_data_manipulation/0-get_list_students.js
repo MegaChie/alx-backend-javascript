d992ab0c624f791e27f356413a57441e4b76966f
MegaChie <workspace.mega@gmail.com>
2024-06-03 04:55:32 +0000

    Basic list of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  const stud = [];
+  stud[0] = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  stud[1] = { id: 2, firstName: 'James', location: 'Columbia' };
+  stud[2] = { id: 3, firstName: 'Serena', location: 'San Francisco' };
+  return stud;
+}
