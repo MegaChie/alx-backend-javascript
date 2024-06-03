536b34d0355e2e0f0944f46805daf9ba54391d4d
MegaChie <workspace.mega@gmail.com>
2024-06-03 21:46:33 +0000

    Fix typo

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,7 +3,7 @@ import getListStudents from './0-get_list_students';
 export default function updateStudentGradeByCity(studs = getListStudents(), city, newGrades) {
   return studs.filter((elem) => elem.location === city)
     .map((human) => {
-      const grades = newGrades.filter((elem) => elem.studID === human.id);
+      const grades = newGrades.filter((elem) => elem.studentId === human.id);
       const grade = grades.length ? grades[0].grade : 'N/A';
       return {
         ...human,
