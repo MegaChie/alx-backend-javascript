f291ded358ddd7c2a5e32f9ae94998ba508cd451
MegaChie <workspace.mega@gmail.com>
2024-07-22 19:50:56 +0000

    Prints stats from CSV file and returns a promise

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,32 @@
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.trim().split('\n');
+    if (lines.length === 0) {
+      throw new Error('Cannot load the database');
+    }
+
+    const students = lines.slice(1).filter((line) => line.trim() !== '');
+    const studentsCount = students.length;
+    console.log(`Number of students: ${studentsCount}`);
+
+    const fields = {};
+    for (const student of students) {
+      const [fname, , , field] = student.split(',');
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(fname);
+    }
+
+    for (const [field, fname] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${fname.length}. List: ${fname.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
