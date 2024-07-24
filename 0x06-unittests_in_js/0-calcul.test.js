a6cbe9b56ca480d609e06a2adf91e685f2de1bbf
MegaChie <workspace.mega@gmail.com>
2024-07-24 12:24:13 +0000

    Adds more tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -21,4 +21,24 @@ describe('numberAddition', () => {
     const res = sumNumber(1.5, 3.8);
     assert.strictEqual(res, 6);
   });
+
+  it('round number and add (1.0, 0)', () => {
+    const res = sumNumber(1.0, 0);
+    assert.strictEqual(res, 1);
+  });
+
+  it('round number and add (0, 0)', () => {
+    const res = sumNumber(0, 0);
+    assert.strictEqual(res, 0);
+  });
+
+  it('round number and add (10, 10.6)', () => {
+    const res = sumNumber(10, 10.6);
+    assert.strictEqual(res, 21);
+  });
+
+  it('round number and add (100, 100.6)', () => {
+    const res = sumNumber(100, 100.6);
+    assert.strictEqual(res, 201);
+  });
 });
