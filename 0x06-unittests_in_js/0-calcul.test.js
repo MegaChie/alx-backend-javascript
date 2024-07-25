168e7989ba69b93caf5ef47b432ba5bdc220b388
MegaChie <workspace.mega@gmail.com>
2024-07-25 16:51:25 +0000

    Trial and error

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,44 +1,68 @@
 const assert = require('assert');
-const sumNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
-describe('numberAddition', () => {
-  it('round number and add (1, 3)', () => {
-    const res = sumNumber(1, 3);
-    assert.strictEqual(res, 4);
+describe('calculateNumber', () => {
+  it('sum of two positive int', () => {
+    assert.strictEqual(calculateNumber(2, 5), 7);
   });
-
-  it('round number and add (1, 3.7)', () => {
-    const res = sumNumber(1, 3.7);
-    assert.strictEqual(res, 5);
+  it('sum of float un integer', () => {
+    assert.strictEqual(calculateNumber(2, 4.6), 7);
   });
-
-  it('round number and add (1.2, 3.7)', () => {
-    const res = sumNumber(1.2, 3.7);
-    assert.strictEqual(res, 5);
+  it('sum of two positive float with round half up', () => {
+    assert.strictEqual(calculateNumber(2.5, 4.6), 8);
   });
-
-  it('round number and add (1.5, 3.8)', () => {
-    const res = sumNumber(1.5, 3.8);
-    assert.strictEqual(res, 6);
+  it('sum of two positive float with round half down', () => {
+    assert.strictEqual(calculateNumber(2.3, 4.4), 6);
   });
-
-  it('round number and add (1.0, 0)', () => {
-    const res = sumNumber(1.0, 0);
-    assert.strictEqual(res, 1);
+  it('sum of two positive float with round half down and other up', () => {
+    assert.strictEqual(calculateNumber(2.3, 4.7), 7);
   });
-
-  it('round number and add (0, 0)', () => {
-    const res = sumNumber(0, 0);
-    assert.strictEqual(res, 0);
+  it('sum of integer, positive and negative', () => {
+    assert.strictEqual(calculateNumber(2, -4), -2);
   });
-
-  it('round number and add (10, 10.6)', () => {
-    const res = sumNumber(10, 10.6);
-    assert.strictEqual(res, 21);
+  it('sum of two negative int', () => {
+    assert.strictEqual(calculateNumber(-2, -4), -6);
   });
-
-  it('round number and add (100, 100.6)', () => {
-    const res = sumNumber(100, 100.6);
-    assert.strictEqual(res, 201);
+  it('sum of two negative float with round half up', () => {
+    assert.strictEqual(calculateNumber(-2.5, -4.6), -7);
+  });
+  it('sum of two negative float with round half down', () => {
+    assert.strictEqual(calculateNumber(-2.3, -4.4), -6);
+  });
+  it('sum of two negative float with round half down and other up', () => {
+    assert.strictEqual(calculateNumber(-2.3, -4.7), -7);
+  });
+  it('first parameters is a string', () => {
+    assert.strictEqual(calculateNumber('2', 4), 6);
+  });
+  it('second parameters is a string', () => {
+    assert.strictEqual(calculateNumber(2, '4'), 6);
+  });
+  it('both parameters are strings', () => {
+    assert.strictEqual(calculateNumber('2', '4'), 6);
+  });
+  it('both parameters are strings, one of them in float format', () => {
+    assert.strictEqual(calculateNumber('2', '4.6'), 7);
+  });
+  it('both parameters are strings in float format', () => {
+    assert.strictEqual(calculateNumber('2.5', '4.6'), 8);
+  });
+    it('one of parameters is missing', () => {
+    assert.strictEqual(calculateNumber(2), NaN);
+  });
+  it('both parameters is missing', () => {
+    assert.strictEqual(calculateNumber(), NaN);
+  });
+  it('one of parameters is NaN', () => {
+    assert.strictEqual(calculateNumber(NaN, 4), NaN);
+  });
+  it('both parameters is NaN', () => {
+    assert.strictEqual(calculateNumber(NaN, NaN), NaN);
+  });
+  it('one of parameters is Infinity', () => {
+    assert.strictEqual(calculateNumber(Infinity, 4), Infinity);
+  });
+  it('both parameters is Infinity', () => {
+    assert.strictEqual(calculateNumber(Infinity, Infinity), Infinity);
   });
 });
