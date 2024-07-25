b2d26737b70c1b813b030277b7907bb5f4921e0c
MegaChie <workspace.mega@gmail.com>
2024-07-25 16:56:28 +0000

    Trial and error

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,12 @@
+/**
+ * Round a and b and return the sum
+ * @param {number} a - The first number.
+ * @param {number} b - the second number.
+ * @returns {number} return the sum.
+ */
+
 function calculateNumber(a, b) {
-  const x = Math.round(a);
-  const y = Math.round(b);
-  return x + y;
+  return Math.round(a) + Math.round(b);
 }
 
 module.exports = calculateNumber;
