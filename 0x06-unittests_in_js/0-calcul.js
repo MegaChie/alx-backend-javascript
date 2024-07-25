326728064af787b479415548fb364f1d24808850
MegaChie <workspace.mega@gmail.com>
2024-07-25 17:38:26 +0000

    Resets to normal

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,12 +1,7 @@
-/**
- * Round a and b and return the sum
- * @param {number} a - The first number.
- * @param {number} b - the second number.
- * @returns {number} return the sum.
- */
-
 function calculateNumber(a, b) {
-  return Math.round(a) + Math.round(b);
+  const x = Math.round(a);
+  const y = Math.round(b);
+  return x + y;
 }
 
 module.exports = calculateNumber;
